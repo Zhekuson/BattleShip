b11b637e05356033cb7304438e1b8c2957c116d8
Zhekuson <zhekuson@gmail.com>
2019-10-07 00:14:50 +0300

    Ended 1 day

--- src/battleship/ocean/Ocean.java
+++ src/battleship/ocean/Ocean.java
@@ -4,16 +4,49 @@ import battleship.ships.EmptySea;
 import battleship.ships.Ship;
 
 public class Ocean {
-    public static final int  FIELDSIZE = 10;
+    public static final int FIELDSIZE = 10;
     Ship[][] ships = new Ship[FIELDSIZE][FIELDSIZE];
     int shotsFired;
     int hitCount;
     int shipsSunk;
-    Ocean(){
+    public Ocean(){
         for (int i = 0; i < FIELDSIZE; i++) {
             for (int j = 0; j < FIELDSIZE; j++){
                 ships[i][j] = new EmptySea();
             }
         }
     }
+
+    void placeAllShipsRandomly(){
+
+    }
+    boolean isOccupied(int row, int column){
+
+    }
+    boolean shootAt(int row, int column){
+
+    }
+    int getShotsFired(){
+
+    }
+    int getHitCount(){
+
+    }
+    int getShipsSunk(){
+
+    }
+    boolean isGameOver(){
+
+    }
+    public Ship[][] getShipArray(){
+        return ships;
+    }
+
+    /**
+     * Prints the ocean
+     */
+    void print(){
+
+    }
+
 }
