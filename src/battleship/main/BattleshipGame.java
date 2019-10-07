f3e2f5d7cdc73f232401a904b3053335bd9b5352
Zhekuson <zhekuson@gmail.com>
2019-10-07 14:41:49 +0300

    no message

--- src/battleship/main/BattleshipGame.java
+++ src/battleship/main/BattleshipGame.java
@@ -1,4 +1,5 @@
 package battleship.main;
+import battleship.ocean.Ocean;
 import battleship.ships.*;
 
 /**
@@ -6,7 +7,11 @@ import battleship.ships.*;
  *
  */
 public class BattleshipGame {
+    public static Ocean ocean;
     public static void main(String[] args) {
+        System.out.println("Hello and welcome to the battleship game");
+        ocean = new Ocean();
+
 
     }
 
--- src/battleship/ocean/Ocean.java
+++ src/battleship/ocean/Ocean.java
@@ -3,20 +3,26 @@ package battleship.ocean;
 import battleship.ships.EmptySea;
 import battleship.ships.Ship;
 
+import java.io.Console;
+
 public class Ocean {
     public static final int FIELDSIZE = 10;
+    public static final int SHIPCOUNT = 10;
     Ship[][] ships = new Ship[FIELDSIZE][FIELDSIZE];
     int shotsFired;
     int hitCount;
     int shipsSunk;
     public Ocean(){
+        initializeEmptySea();
+
+    }
+    void initializeEmptySea(){
         for (int i = 0; i < FIELDSIZE; i++) {
             for (int j = 0; j < FIELDSIZE; j++){
                 ships[i][j] = new EmptySea();
             }
         }
     }
-
     void placeAllShipsRandomly(){
 
     }
@@ -38,7 +44,7 @@ public class Ocean {
         return shipsSunk;
     }
     boolean isGameOver(){
-        
+        return shipsSunk == SHIPCOUNT;
     }
     public Ship[][] getShipArray(){
         return ships;
@@ -48,7 +54,12 @@ public class Ocean {
      * Prints the ocean
      */
     void print(){
-
+        for (int i = 0; i < FIELDSIZE; i++){
+            for(int j = 0; j < FIELDSIZE; j++){
+                System.out.print(ships[i][j]);
+            }
+            System.out.println();
+        }
     }
 
 }
