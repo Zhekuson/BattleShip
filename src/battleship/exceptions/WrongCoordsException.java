6abd3c8d1736045a88891c8b7f998e2abf212b1e
Zhekuson <zhekuson@gmail.com>
2019-10-13 17:46:31 +0300

    changed some things, added jar

--- src/battleship/exceptions/WrongCoordsException.java
+++ src/battleship/exceptions/WrongCoordsException.java
@@ -1,7 +1,15 @@
 package battleship.exceptions;
 
+/**
+ * custom exception for wrong row & column on input
+ */
 public class WrongCoordsException extends Exception {
     public WrongCoordsException(){}
+
+    /**
+     *  constructor for functionality
+     * @param message
+     */
     public WrongCoordsException(String message)
     {
         super(message);
